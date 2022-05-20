1beb908f26f8fc86c324f52b2912b667cb10749a
fangt11 <fangt11@ke.com>
2022-05-20 15:11:22 +0800

    docs: nothing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import 'antd/dist/antd.css';
 import 'antd/dist/antd.dark.css';
-import '@ke/table/dist/index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
