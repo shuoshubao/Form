9faa2b633a841c6f294ffee4b1c3f18f05b697d1
fangt11 <fangt11@ke.com>
2022-05-20 15:11:12 +0800

    feat: 新增 tpl: upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
+import '@ke/table/dist/index.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
