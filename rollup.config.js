e64d2c3bece0fbe826e4ab5753af5bf20df1cf45
fangt11 <fangt11@ke.com>
2022-05-20 14:27:25 +0800

    feat: 将内置子组件抽离到components.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,37 @@
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 
-export default {
-    input: 'lib/index.js',
-    output: {
-        file: 'dist/index.js',
-        format: 'cjs',
-        exports: 'default'
-    },
-    external: id => {
-        return !['.', '/'].some(v => id.startsWith(v));
-    },
-    plugins: [
-        postcss({ extract: true }),
-        babel({
-            babelrc: false,
-            babelHelpers: 'bundled',
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties']
-        })
-    ]
+const babelConfig = {
+    babelrc: false,
+    babelHelpers: 'bundled',
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: ['@babel/plugin-proposal-class-properties']
 };
+
+export default [
+    {
+        input: 'lib/index.js',
+        output: {
+            file: 'dist/index.js',
+            format: 'cjs',
+            exports: 'default'
+        },
+        plugins: [postcss({ extract: true }), babel(babelConfig)]
+    },
+    {
+        input: 'lib/components.js',
+        output: {
+            file: 'dist/components.js',
+            format: 'cjs'
+        },
+        plugins: [babel(babelConfig)]
+    },
+    {
+        input: 'lib/components.js',
+        output: {
+            file: 'dist/components.esm.js',
+            format: 'esm'
+        },
+        plugins: [babel(babelConfig)]
+    }
+];
