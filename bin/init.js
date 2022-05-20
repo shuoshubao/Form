9d189a724a35e0ae813343fadd00fbb98c46023d
fangt11 <fangt11@ke.com>
2022-05-20 15:10:14 +0800

    ci: remove Render.js

--- bin/init.js
+++ bin/init.js
@@ -2,12 +2,9 @@
  * @Author: fangt11
  * @Date:   2022-03-07 12:15:50
  * @Last Modified by:   fangt11
- * @Last Modified time: 2022-03-07 14:19:46
+ * @Last Modified time: 2022-03-07 14:22:41
  */
 const { readFileSync, writeFileSync } = require('fs');
-const { copySync } = require('fs-extra');
-
-copySync('node_modules/@ke/table/dist/Render.esm.js', 'lib/Render.js');
 
 writeFileSync(
     './lib/v3.js',
